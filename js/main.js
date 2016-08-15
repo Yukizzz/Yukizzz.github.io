403040bdc7b04cfeae93cf6a60ca456e5a2ccae2
Yukizzz <mariozzzjyz@outlook.com>
2016-08-15 12:55:37 +0800

    Site updated: 2016-08-15 12:55:35

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 require([], function (){
 
+    var jsPath = yiliaConfig.rootUrl + "js\/"
     var isMobileInit = false;
     var loadMobile = function(){
-        require(['/js/mobile.js'], function(mobile){
+        require([jsPath + 'mobile.js'], function(mobile){
             mobile.init();
             isMobileInit = true;
         });
     }
     var isPCInit = false;
     var loadPC = function(){
-        require(['/js/pc.js'], function(pc){
+        require([jsPath + 'pc.js'], function(pc){
             pc.init();
             isPCInit = true;
         });
@@ -47,23 +48,38 @@ require([], function (){
         }
     });
 
-    if(browser.versions.mobile === true || $(window).width() < 700){
+    if(browser.versions.mobile === true || $(window).width() < 800){
         loadMobile();
     }else{
         loadPC();
     }
 
+    resetTags = function(){
+        var tags = $(".tagcloud a");
+        for(var i = 0; i < tags.length; i++){
+            var num = parseInt(7*Math.random());
+            tags.eq(i).addClass("color" + num);
+        };
+        $(".article-category a:nth-child(-n+2)").attr("class", "color0");
+    }
+
     //是否使用fancybox
     if(yiliaConfig.fancybox === true){
-        require(['/fancybox/jquery.fancybox.js'], function(pc){
+        require([yiliaConfig.fancybox_js], function(pc){
             var isFancy = $(".isFancy");
             if(isFancy.length != 0){
                 var imgArr = $(".article-inner img");
                 for(var i=0,len=imgArr.length;i<len;i++){
                     var src = imgArr.eq(i).attr("src");
                     var title = imgArr.eq(i).attr("alt");
-                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
+                    if(typeof(title) == "undefined"){
+                        var title = imgArr.eq(i).attr("title");
+                    }
+                    var width = imgArr.eq(i).attr("width");
+                    var height = imgArr.eq(i).attr("height");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' width="+width+" height="+height+" title='"+title+"' alt='"+title+"'></a>");
                 }
+
                 $(".article-inner .fancy-ctn").fancybox();
             }
         });
@@ -72,21 +88,10 @@ require([], function (){
     //是否开启动画
     if(yiliaConfig.animate === true){
 
-        require(['/js/jquery.lazyload.js'], function(){
-            //avatar
-            $(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
-            $(".js-avatar")[0].onload = function(){
-                $(".js-avatar").addClass("show");
-            }
-        });
-
       if(yiliaConfig.isHome === true) {
         // 滚动条监听使用scrollreveal.js
         // https://github.com/jlmakes/scrollreveal.js
-        // 使用cdn[//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js]
-        require([
-          '//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js'
-        ], function (ScrollReveal) {
+        require([yiliaConfig.scrollreveal], function (ScrollReveal) {
           // 更多animation:
           // http://daneden.github.io/animate.css/
           var animationNames = [
@@ -119,6 +124,12 @@ require([], function (){
               }
               return;
           }
+          var animateScope = ".body-wrap > article";
+          var $firstArticle = $(".body-wrap > article:first-child");
+          if ($firstArticle.height() > $(window).height()) {
+              var animateScope = ".body-wrap > article:not(:first-child)";
+              $firstArticle.css({opacity: 1});
+          }
           // document.body有些浏览器不支持监听scroll，所以使用默认的document.documentElement
           ScrollReveal({
             duration: 0,
@@ -126,36 +137,97 @@ require([], function (){
               // safari不支持requestAnimationFrame不支持document.documentElement的onscroll所以这里不会执行
               // 初始状态设为opacity: 0, 动画效果更平滑一些(由于脚本加载是异步，页面元素渲染后在执行动画，感觉像是延时)
               $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1});
-            }
-          }).reveal('.body-wrap > article');
-
+              }
+            }).reveal(animateScope);
         });
       } else {
         $('.body-wrap > article').css({opacity: 1});
       }
-
     }
 
-    //是否新窗口打开链接
-    if(yiliaConfig.open_in_new == true){
-        $(".article a[href]").attr("target", "_blank")
+    // TOC
+    if (yiliaConfig.toc) {
+        require(['toc'], function(){ })
     }
-    $(".archive-article-title, .github-widget a").attr("target", "_blank");
 
-    //随机颜色
+    // 随机颜色
     var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
     var id = Math.ceil(Math.random()*(colorList.length-1));
-    //PC
+    // PC
     $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
-    //移动端
+    // Mobile
     $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
 
     $("table").wrap("<div class='table-area'></div>");
 
-    var tags = $(".tagcloud a");
-    for(var i = 0; i < tags.length; i++){
-        var num = parseInt(4*Math.random()) + 1;
-        tags.eq(i).addClass("color" + num);
+    // Hide Comment Button
+    $(document).ready(function() {
+        if ($("#comments").length < 1) {
+            $("#scroll > a:nth-child(2)").hide();
+        };
+    })
+
+    // Hide Labels
+    if(yiliaConfig.isArchive || yiliaConfig.isTag || yiliaConfig.isCategory) {
+        $(document).ready(function() {
+            $("#footer").after("<button class='hide-labels'>TAGS</button>");
+            $(".hide-labels").click(function() {
+                $(".article-info").toggle(200);
+            });
+        });
     }
-    $(".article-category a:nth-child(-n+2)").attr("class", "color5");
-});
+
+    // text-block
+    $(document).ready(function() {
+        var highlight = {
+            background: $(".article-entry .highlight").css("background"),
+            foreground: $(".article-entry .highlight").css("color")
+        }
+        $(".article-entry > pre").css("background-color", highlight.background);
+        $(".article-entry > pre code").css("color", highlight.foreground);
+    });
+
+    // Task lists in markdown
+    $('ul > li').each(function() {
+        var taskList = {
+            field: this.textContent.substring(0, 2),
+            check: function(str) {
+                var re = new RegExp(str);
+                return this.field.match(re);
+            }
+        }
+
+        var string = ["[ ]", ["[x]", "checked"]];
+        var checked = taskList.check(string[1][0]);
+        var unchecked = taskList.check(string[0]);
+
+        var $current = $(this);
+        function update(str, check) {
+            var click = ["disabled", ""];
+            $current.html($current.html().replace(
+              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+            )
+        }
+
+        if (checked || unchecked) {
+            this.classList.add("task-list");
+            if (checked) {
+                update(string[1][0], string[1][1]);
+                this.classList.add("check");
+            } else {
+                update(string[0], "");
+            }
+        }
+    })
+
+})
+
+
+
+/*var autoKeyword = [
+  "Hexo",
+  "Markdown",
+];
+$( "#local-search-input" ).autocomplete({
+  source: autoKeyword
+});*/
